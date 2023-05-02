f8269792025fa058399286b56eb3fac127619662
QuantumBen <omosabenedict@gmail.com>
2023-05-02 09:23:42 +0100

    Add Post, model,factory, seeder and migration

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Provider\Image;
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $imagePath = 'public/fake_images';
+
+        // Create the directory if it doesn't exist
+        if (!File::exists($imagePath)) {
+            File::makeDirectory($imagePath);
+        }
+
+        return [
+            'title' => fake()->city(),
+            'description' => fake()->sentence(30),
+            'image' => Image::image($imagePath, 150, 150, 'post', false),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
        $this->call(MemberSeeder::class);
-
+       $this->call(PostSeeder::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
